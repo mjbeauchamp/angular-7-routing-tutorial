3b7700fde430255a1526560df27d23f6670c1af7
Monica Beauchamp <monica.beauchamp@fmr.com>
2019-05-17 14:28:53 -0600

    error handling

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
-import {ActivatedRoute, Params} from '@angular/router';
+import {ActivatedRoute, Params, Router} from '@angular/router';
 import {query} from '@angular/animations';
+import {Observable} from 'rxjs/Observable';
 
 @Component({
   selector: 'app-edit-server',
@@ -15,9 +16,11 @@ export class EditServerComponent implements OnInit {
   serverStatus = '';
   queryParams = {};
   allowEdit = false;
+  changesSaved = false;
 
   constructor(private serversService: ServersService,
-              private activeRoute: ActivatedRoute) { }
+              private activeRoute: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit() {
     this.queryParams = this.activeRoute.snapshot.queryParams;
@@ -28,13 +31,27 @@ export class EditServerComponent implements OnInit {
         }
       );
     this.activeRoute.fragment.subscribe();
-    this.server = this.serversService.getServer(1);
+    const id = +this.activeRoute.snapshot.params['id'];
+    this.server = this.serversService.getServer(id);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
   }
 
   onUpdateServer() {
     this.serversService.updateServer(this.server.id, {name: this.serverName, status: this.serverStatus});
+    this.changesSaved = true;
+    this.router.navigate(['../'], {relativeTo: this.activeRoute});
+  }
+
+  canDeactivate(): Observable<boolean> | Promise<boolean> | boolean{
+    if (!this.allowEdit) {
+      return true;
+    }
+    if ((this.serverName !== this.server.name || this.serverStatus !== this.server.status) && !this.changesSaved) {
+      return confirm('Do you want to discard the changes?')
+    } else {
+      return true;
+    }
   }
 
 }
